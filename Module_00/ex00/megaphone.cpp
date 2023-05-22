6f776e32f045d1513f327512b658c0c6bef43281
Marco Vomiero <mvomiero@student.42berlin.de>
2023-05-22 13:45:42 +0200

    Module_00/ex00 done

---
+++ Module_00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 13:34:53 by mvomiero          #+#    #+#             */
+/*   Updated: 2023/05/22 13:43:44 by mvomiero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstdlib>
+
+int	main(int ac, char **av) {
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else {
+		for (int i = 1; av[i]; i++) {
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char) toupper(av[i][j]);
+			if (i < ac - 1)
+				std::cout << ' ';
+		}
+	}
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
+}
