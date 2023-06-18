99368e0d25f1491af1c7adc4381f060df6afba6f
Marco Vomiero <colocomarco1@gmail.com>
2023-06-18 15:11:00 +0200

    default tests main

--- Module_04/ex01/main.cpp
+++ Module_04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 17:48:53 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/06/18 13:53:37 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/06/18 15:09:56 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,14 @@
 
 int main( void )
 {
+	/* DEFAULT TESTS */
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	delete j;//should not create a leak
+	delete i;
+
+	/* TESTS MARCO */
 	// setting two elements Cat and Dog to assign an idea.
 	Cat* frontman = new Cat;
 	frontman->setIdea( 0, BLUE "Welcome to our deep concert" RESET ); // to show they use their ideas (setting the idea)
@@ -31,6 +39,7 @@ int main( void )
 	backman->setIdea( 0, BLUE "Thank you for being there" RESET ); // to show they use their ideas (setting the idea)
 
 	const Animal* animals[4] = { frontman, new Dog(), new Cat(), backman };
+
 	std::cout << "\n\t🧠 ZOO CONCERT: 🧠\n" << std::endl;
 	std::cout << frontman->getType() << ": " << frontman->getIdea( 0 ) << "\n" << std::endl; // display the idea we set before
 	for ( int i = 0; i < 4; i++ ) {
