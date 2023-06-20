0ccf344e12ecd6a3e84d3a19cb4c8c751f159619
Marco Vomiero <mvomiero@student.42berlin.de>
2023-06-20 13:35:06 +0200

    update comment main

--- Module_04/ex03/main.cpp
+++ Module_04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 12:28:44 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/06/20 12:51:10 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/06/20 13:32:21 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,22 @@
 #include "Ice.hpp"
 #include "Cure.hpp"
 
+/* Let's crack down all this classes of this a bit complex exercise:
+	-- Amateria: abstract class to implement the Materia classes Ice and Cure
+		No memory allocation for this three classes, just the .clone() function returns
+		a 
+ */
+
 int main()
 {
-	std::cout << VERBOSE << std::endl;
-	//std::cout << CIAO << std::endl;
+	if (VERBOSE) { std::cout << "\nVerbose mode on\n" << std::endl; }
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
 
 	ICharacter* me = new Character("me");
 
-	AMateria* tmp;
+	AMateria* tmp; // practical because an Amateria pointer I can assign it both to Ice and Cure objects
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
