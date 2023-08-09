67af2fb3c2dec14686e5265b55f4fb81b25da837
Marco Vomiero <colocomarco1@gmail.com>
2023-08-09 17:34:27 +0200

    commented

--- Module_05/ex01/main.cpp
+++ Module_05/ex01/main.cpp
@@ -6,33 +6,14 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 12:13:09 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/08/09 17:08:41 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/08/09 17:33:14 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* ABSTRACT:
-	exceptions using try and catch, creating a custom subclass derived from std::exceptions
-*/
-
-/* EXCEPTIONS:
-	-- Exceptions in C++ are a powerful mechanism for handling unexpected or erroneous
-		conditions that may arise during program execution.
-	-- Controlled manner to respond to errors, instead of crushing the program.
-	-- Lots of predefined classes like std::runtime_errors, std::logic_errors and so on,
-		but you can develop your own ones (like in this exercise).
-	
-	Usage: 
-	-- `try` is used to enclose potentially error-prone code
-	-- `throw` is used to raise exceptions when errors occur
-	-- `catch` is used to handle and recover from those exceptions gracefully.
-
-	you can throw basically anything from an int to these subclasses of std::exception
-	which are the default. the parameter to catch will be a reference of an object of the 
-	same class to correctly catch the right exception. 
-
-	-- what () function: 
-		exceptions normally have a what() function that returns a string giving more
-		informations about the problem that happended (here is overloaded in the subclass)
+	improving the exercise before with a class Form that at initialization is 
+	not "signed", that checks if the bureaucrat's grade is high enough to be signed.
+	If it is, the form will be signed.
 */
 
 #include "Bureaucrat.hpp"
@@ -43,6 +24,7 @@ int main( void )
 {
 
 	try {
+		// the grade is too low, the form won't be signed
 		Bureaucrat bureaucrat("Bureaucrat",11);
 		Form form("Form", 10);
 		std::cout << CYAN BOLD "\n1️⃣ First try\n" RESET << std::endl;
@@ -52,11 +34,17 @@ int main( void )
 
 		bureaucrat.incrementGrade();
 
+		// the bureaucrat's grade is incremented, will be enough to sign the form
 		std::cout << CYAN BOLD "\n2️⃣ Second try\n" RESET << std::endl;
 		bureaucrat.signForm(form);
 		std::cout << form << std::endl;
 		std::cout << BLUE << bureaucrat << RESET << std::endl;
 
+	/* NB:
+		using a standard std::exeception I will intercect all the exceptions, then the what message
+		will give me more information about what happened. the problem is if you would have
+		more than one exception, it would intercect just the first one.
+		So: this is not the best approach but still can be practical */
 	} catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
