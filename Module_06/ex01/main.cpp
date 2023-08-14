3e4d490705714dbe11b7c023879ac6756777b446
Marco Vomiero <colocomarco1@gmail.com>
2023-08-14 17:39:06 +0200

    ex02 commented

--- Module_06/ex01/main.cpp
+++ Module_06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 16:56:36 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/08/14 17:12:15 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/08/14 17:13:24 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 /* ABSTRACT:
 	Creating the Serializer static class with the methods serialize() and deserialize()
 	to reinterpret a pointer value to int and vice versa using reinterpret_cast.
-
 	-- serialize() -> from pointer to int
 	-- deserialize() -> from int to pointer
  */
--- Module_06/ex02/main.cpp
+++ Module_06/ex02/main.cpp
@@ -6,20 +6,52 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 17:08:06 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/08/13 17:24:34 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/08/14 17:38:56 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "colors.h"
 
+/* ABSTRACT:
+	two functions to generate and identify an object:
+	-- generate() just returns a pointer to a new element of the derived class
+	-- identify() -> static polymorphism depending the input, which is either
+		a pointer of a reference.
+		With this two functions we see how dynamic_casting can be useful to identify
+		the class of a given element.
+		-- identify( pointer ) --> tries the casting, if it fails the pointer is NULL
+			no problems because is downcastinc, so from the base class to the subclass.
+			if it goes wrong, just the pointer is null
+		-- identify( reference ) --> tries the casting, but since the input is
+			a reference to a derived class, if you try the casting and is not of the same
+			type, a std::bad_casting exception is thrown and the program crushes.
+			That's why "try" and "catch" are used to handle exception.
+ */
+
+/* DYNAMIC CASTING:
+	only type of casting that works dynamically, with polymorphic classes (have 
+	at least one virtual function).
+	Use cases:
+	-- DOWNCASTING: the base pointer is set to an derived-class element
+	(something we already did in some excercises. An example here:
+		Base* basePtr = new Derived;
+		Derived* derivedPtr = dynamic_cast<Derived*>(basePtr);
+	-- some other smaller cases
+	-- RETURN VALUE: is NULL if the casting fails, by reference an exception is thrown!
+ */
+
+/* Base classes and derived classes
+ */
 class Base { public: virtual ~Base() {} };
 class A : public Base {};
 class B : public Base {};
 class C : public Base {};
 
-
-Base *	generate(void)
+/* generate():
+	to generate a random derived class object
+ */
+Base *	generate( void )
 {
 	std::cout << "Generated: ";
 	int	rnb = std::rand() % 3;
@@ -35,10 +67,12 @@ Base *	generate(void)
 			std::cout << PURPLE "C" RESET;
 			return (new C);
 	}
-	return (NULL);
+	return (NULL); // Just because required from the compiler using the switch
 }
 
-void	identify(Base * p)
+/* to identify a Base* pointer, so to check if it's A, B, or C
+ */
+void	identify( Base * p )
 {
 	std::cout << "Identified as pointer: ";
 	if (dynamic_cast<A *>(p))
@@ -51,10 +85,11 @@ void	identify(Base * p)
 		std::cout << RED "Unknown" RESET;
 }
 
-
-/* if you dont use the try and catch the program would stop with a std::bad_cast exception!
-	This way, the execution goes on */
-void    identify( Base& p ) {
+/* to identify an object, so a reference.
+	NB Exceptions:	if you dont use the try and catch the program would stop with
+	 a std::bad_cast exception! This way, the execution goes on
+ */
+void	identify( Base& p ) {
 	std::cout << "Identified as reference: ";
 	try {
 		A& a = dynamic_cast< A& >( p );
@@ -73,6 +108,8 @@ void    identify( Base& p ) {
 	} catch( const std::exception& e ) {}
 }
 
+/* just creates random objects and then identifies them by pointer or by reference
+ */
 int	main(void)
 {
 	Base *	p;
