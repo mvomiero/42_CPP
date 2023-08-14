e0e4b2c21cf9f6348f87912cc0bbb7be67671949
Marco Vomiero <colocomarco1@gmail.com>
2023-08-14 13:52:17 +0200

    ex00 completed

--- Module_06/ex00/ScalarConverter.cpp
+++ Module_06/ex00/ScalarConverter.cpp
@@ -1,34 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/12 16:31:26 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/08/14 13:48:39 by mvomiero         ###   ########.fr       */
+/*   Created: 2023/08/14 13:28:35 by mvomiero          #+#    #+#             */
+/*   Updated: 2023/08/14 13:47:20 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include "Converter.hpp"
 #include "ScalarConverter.hpp"
 
-int main( int ac, char ** av )
-{
-	if ( ac != 2 ) {
-		std::cout << "Usage: ./convert number" << std::endl;
-		return EXIT_FAILURE;
+
+/* class ScalarConverter {
+public:
+	// Static function to perform the conversion and printing
+	static void convertAndPrint(const std::string& str) {
+		Converter converter;
+		converter.setStr(str);
+		converter.convert();
+		std::cout << converter << std::endl;
+	}
+}; */
+
+void ScalarConverter::convertAndPrint(const std::string& str) {
+		Converter converter;
+		converter.setStr(str);
+		converter.convert();
+		std::cout << converter << std::endl;
 	}
 
-	try
-	{
-		ScalarConverter::convertAndPrint(av[1]);
-	}
-	catch( const std::exception& e ) {
-		std::cerr << "Error: " << e.what() << std::endl;
-	}
-	
 
-	return EXIT_SUCCESS;
-}
--- Module_06/ex00/ScalarConverter.hpp
+++ Module_06/ex00/ScalarConverter.hpp
@@ -1,34 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/12 16:31:26 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/08/14 13:48:39 by mvomiero         ###   ########.fr       */
+/*   Created: 2023/08/14 13:28:58 by mvomiero          #+#    #+#             */
+/*   Updated: 2023/08/14 13:52:03 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
 #include "Converter.hpp"
-#include "ScalarConverter.hpp"
 
-int main( int ac, char ** av )
-{
-	if ( ac != 2 ) {
-		std::cout << "Usage: ./convert number" << std::endl;
-		return EXIT_FAILURE;
-	}
+class ScalarConverter {
+private:
+	ScalarConverter();
+	~ScalarConverter();
+	ScalarConverter(const ScalarConverter& other);
+	ScalarConverter& operator=(const ScalarConverter& other);
 
-	try
-	{
-		ScalarConverter::convertAndPrint(av[1]);
-	}
-	catch( const std::exception& e ) {
-		std::cerr << "Error: " << e.what() << std::endl;
-	}
-	
-
-	return EXIT_SUCCESS;
-}
+public:
+	static void convertAndPrint(const std::string& str);
+};
