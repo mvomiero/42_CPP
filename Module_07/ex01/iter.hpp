128cb3e3c26359d1c7644136980a964cc5130eb2
Marco Vomiero <mvomiero@student.42berlin.de>
2023-10-11 15:57:01 +0200

    ex01 working

--- Module_07/ex01/iter.hpp
+++ Module_07/ex01/iter.hpp
@@ -6,27 +6,17 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 13:00:56 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/09/10 13:13:49 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:49:38 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
 
-template < typename T >
-void	iter( T* array, size_t size, void( *f ) ( T& )) {
-	for ( size_t i = 0; i < size; i++ )
-		f( array[i] );
-}
-
-// a print function to do some testing
-template < typename T >
-void	print( T& i ) {
-	std::cout << i << std::endl;
-}
-
-// an increment function to do some testing
-template < typename T >
-void	increment( T& i ) {
-	i++;
+template <typename A, typename F>
+void	iter(A * array, size_t length, F function)
+{
+	for (size_t i = 0; i < length; i++)
+		function(array[i]);
+	return ;
 }
--- Module_07/ex01/main.cpp
+++ Module_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 13:06:37 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/09/10 13:55:43 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:56:39 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,34 +24,78 @@
 	you get a compilation error).
  */
 
+/*// a print function to do some testing
+template < typename T >
+void	print( T& i ) {
+	std::cout << i << std::endl;
+}
+
+// an increment function to do some testing
+template < typename T >
+void	increment( T& i ) {
+	i++;
+}
+
 int	main( void ) {
 
 	int numbers[] = { 1, 2, 3, 4 };
 	std::cout << BLUE "numbers:" RESET << std::endl;
-	iter(numbers, SIZE, &print);
+	iter(numbers, SIZE, print< int >);
 	std::cout << BLUE "numbers increment:" RESET << std::endl;
-	iter(numbers, SIZE, &increment);
-	iter(numbers, SIZE, &print);
+	iter(numbers, SIZE, increment< int >);
+	iter(numbers, SIZE, print< int >);
 
 	double fractionals[] = { 1.2, 2.3, 3.4, 4.5 };
 	std::cout << BLUE "fractionals:" RESET << std::endl;
-	iter(fractionals, SIZE, &print);
+	iter(fractionals, SIZE, print< double >);
 	std::cout << BLUE "fractionals increment:" RESET << std::endl;
-	iter(fractionals, SIZE, &increment);
-	iter(fractionals, SIZE, &print);
+	iter(fractionals, SIZE, increment< double >);
+	iter(fractionals, SIZE, print< double >);
 
 	char characters[] = { 'a', 'b', 'c', 'd' };
 	std::cout << BLUE "characters:" RESET << std::endl;
-	iter(characters, SIZE, &print);
+	iter(characters, SIZE, print< char >);
 	std::cout << BLUE "characters increment:" RESET << std::endl;
-	iter(characters, SIZE, &increment);
-	iter(characters, SIZE, &print);
+	iter(characters, SIZE, increment< char >);
+	iter(characters, SIZE, print< char >);
 
 	std::string strings[] = { "ciao", "come", "va", "marco" };
 	std::cout << BLUE "strings:" RESET << std::endl;
-	iter(strings, SIZE, &print);
+	iter(strings, SIZE, print< std::string >);
 	// the increment doesen't work for the strings, you get a compilation error!
 	// std::cout << BLUE "strings increment:" RESET << std::endl;
 	// iter(strings, SIZE, &increment);
 	
+}*/
+
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
+}
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4 };
+  Awesome tab2[5];
+
+  iter( tab, 5, print<const int> );
+  iter( tab2, 5, print<Awesome> );
+
+  return 0;
 }
