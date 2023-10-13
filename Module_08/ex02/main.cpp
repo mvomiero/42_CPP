b69b12f9bd1d702263fa0b77f877f961ddb3ebd7
Marco Vomiero <mvomiero@student.42berlin.de>
2023-10-13 11:37:55 +0200

    formatting

--- Module_08/ex02/main.cpp
+++ Module_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 18:17:31 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/10/13 11:36:22 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/10/13 11:37:44 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 #include "colors.h"
 #include <vector>
 
-
 template <typename Iterator>
-void printContainer(Iterator begin, Iterator end) {
+void printContainer(Iterator begin, Iterator end)
+{
 	std::cout << "\tContainer: ";
-    while (begin != end) {
-        std::cout << *begin << " ";
-        ++begin;
-    }
+	while (begin != end)
+	{
+		std::cout << *begin << " ";
+		++begin;
+	}
 	std::cout << std::endl;
 }
 
 int main()
 {
 	std::cout << BG_BRIGHT_PURPLE << "------------------- TEST ASSIGNMENT -------------------" << RESET << std::endl;
-	std::cout << PURPLE << "\nMutantStack\n" << RESET << std::endl;
+	std::cout << PURPLE << "\nMutantStack\n"
+			  << RESET << std::endl;
 	{
 		MutantStack<int> mstack;
 		mstack.push(5);
@@ -47,14 +49,15 @@ int main()
 		--it;
 		while (it != ite)
 		{
-		std::cout << *it << std::endl;
-		++it;
+			std::cout << *it << std::endl;
+			++it;
 		}
 		std::stack<int> s(mstack);
-		//return 0;
+		// return 0;
 	}
 	std::cout << BG_BRIGHT_PURPLE << "------------------- TEST ASSIGNMENT -------------------" << RESET << std::endl;
-	std::cout << PURPLE << "\nstd::list\n" << RESET << std::endl;
+	std::cout << PURPLE << "\nstd::list\n"
+			  << RESET << std::endl;
 	{
 		std::list<int> mstack;
 		mstack.push_back(5);
@@ -73,11 +76,11 @@ int main()
 		--it;
 		while (it != ite)
 		{
-		std::cout << *it << std::endl;
-		++it;
+			std::cout << *it << std::endl;
+			++it;
 		}
-		//std::stack<int> s(mstack); // not compatible
-		//return 0;
+		// std::stack<int> s(mstack); // not compatible
+		// return 0;
 	}
 	{
 		std::cout << BG_BRIGHT_BLUE << "\n\tCUSTOM TESTS" << RESET << std::endl;
@@ -85,7 +88,8 @@ int main()
 
 		MutantStack<int> mstack;
 
-		std::cout << PURPLE << "\nCreating a stack with three elements\n" << RESET << std::endl;
+		std::cout << PURPLE << "\nCreating a stack with three elements\n"
+				  << RESET << std::endl;
 		mstack.push(5);
 		mstack.push(17);
 		mstack.push(3);
@@ -94,14 +98,16 @@ int main()
 		printContainer(it, ite);
 
 		std::cout << PURPLE << "\nPopping one element" << RESET << std::endl;
-		std::cout << PURPLE << "\tLIFO strategy: Last In is the First out (a property of stacks)\n" << RESET << std::endl;
+		std::cout << PURPLE << "\tLIFO strategy: Last In is the First out (a property of stacks)\n"
+				  << RESET << std::endl;
 		mstack.pop();
 		std::cout << ITAL << "\tstack top is: " << mstack.top() << " and stack size is: " << mstack.size() << RESET << std::endl;
 		it = mstack.begin(), ite = mstack.end();
 		printContainer(it, ite);
 
 		std::cout << BG_BRIGHT_PURPLE << "\n------------------- TEST 2 -------------------" << RESET << std::endl;
-		std::cout << PURPLE << "\nInstantiating a std::stack with the copy constructor of Mutantstack\n" << RESET << std::endl;
+		std::cout << PURPLE << "\nInstantiating a std::stack with the copy constructor of Mutantstack\n"
+				  << RESET << std::endl;
 		std::stack<int> s(mstack);
 		s.push(42);
 		s.push(21);
@@ -110,28 +116,29 @@ int main()
 		std::cout << PURPLE << "\tcan't print the content since a stack type has no iterators" << RESET << std::endl;
 
 		std::cout << BG_BRIGHT_PURPLE << "\n------------------- TEST 3 -------------------" << RESET << std::endl;
-		std::cout << PURPLE << "\nInstantiating a Mutantstack with the copy constructor\n" << RESET << std::endl;
+		std::cout << PURPLE << "\nInstantiating a Mutantstack with the copy constructor\n"
+				  << RESET << std::endl;
 		MutantStack<int> c = mstack;
 
 		std::cout << ITAL << "\tstack top is: " << c.top() << " and stack size is: " << c.size() << RESET << std::endl;
 		MutantStack<int>::iterator c_it = c.begin(), c_ite = c.end();
 		printContainer(c_it, c_ite);
 
-		
-		
-		//return 0;
+		// return 0;
 	}
 	{
 		std::cout << BG_BRIGHT_PURPLE << "------------------- TEST 4 -------------------" << RESET << std::endl;
-		std::cout << PURPLE << "\nInstantiating a Mutantstack from a std::stack casted from a vector container (instead of the default deque)\n" << RESET << std::endl;
-		MutantStack<int, std::vector<int> > mstack;
+		std::cout << PURPLE << "\nInstantiating a Mutantstack from a std::stack casted from a vector container (instead of the default deque)\n"
+				  << RESET << std::endl;
+		MutantStack<int, std::vector<int>> mstack;
 
-		std::cout << PURPLE << "\nCreating a vector derived stack with three elements\n" << RESET << std::endl;
+		std::cout << PURPLE << "\nCreating a vector derived stack with three elements\n"
+				  << RESET << std::endl;
 		mstack.push(5);
 		mstack.push(17);
 		mstack.push(3);
 		std::cout << ITAL << "\tstack top is: " << mstack.top() << " and stack size is: " << mstack.size() << RESET << std::endl;
-		MutantStack<int, std::vector<int> >::iterator it = mstack.begin(), ite = mstack.end();
+		MutantStack<int, std::vector<int>>::iterator it = mstack.begin(), ite = mstack.end();
 		printContainer(it, ite);
 	}
 	return 0;
