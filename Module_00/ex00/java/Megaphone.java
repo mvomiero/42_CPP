5c323430fa9dd51ef0c7ba4905b5a5d3352e8e55
Marco Vomiero <colocomarco1@gmail.com>
2024-01-20 17:55:18 +0100

    ex00 completed

---
+++ Module_00/ex00/java/Megaphone.java
@@ -0,0 +1,13 @@
+public class Megaphone {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+        } else {
+            for (String arg : args) {
+                System.out.print(arg.toUpperCase());
+            
+            System.out.println();
+        }
+    }
+}
+}
