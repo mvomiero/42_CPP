aa7d3cdbefac7c625708ba342f846cad0b535cee
Marco Vomiero <colocomarco1@gmail.com>
2024-01-21 17:02:26 +0100

    ex00 completed

--- Module_00/ex00/java/Megaphone.java
+++ Module_00/ex00/java/Megaphone.java
@@ -1,11 +1,24 @@
 public class Megaphone {
+    private static String[] uppercaseArgs;
+
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
         } else {
+            uppercaseArgs = new String[args.length];
+
+            for (int i = 0; i < uppercaseArgs.length; i++) {
+                uppercaseArgs[i] = args[i].toUpperCase();
+            }
             for (String arg : args) {
                 System.out.print(arg.toUpperCase());
                 System.out.println();
+            }
+            for (String arg : args) {
+                System.out.print("original str: " + arg + "\n");
+            }
+            for (String arg : uppercaseArgs) {
+                System.out.print("uppercase str: " + arg + "\n");
             }
         }
     }
