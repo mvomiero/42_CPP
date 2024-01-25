5e813eeba8ea904bea8f278a6abffb042325c605
Marco Vomiero <colocomarco1@gmail.com>
2024-01-25 11:17:03 +0100

    work in progress

--- src/Module_01/ex00/java/Main.java
+++ src/Module_01/ex00/java/Main.java
@@ -4,7 +4,10 @@ import consoleUtils.ConsoleColors;
 public class Main {
     public static void main(String[] args) {
         Zombie zombie = new Zombie("Zozo", 20);
+        Zombie zombie2 = new Zombie(/* "Zozo", 20 */);
         System.out.println(zombie);
         ConsoleColors.printRed(zombie.toString());
+        System.out.println(zombie2);
+        ConsoleColors.printRed(zombie2.toString());
     }
 }
--- src/Module_01/ex00/java/Zombie.java
+++ src/Module_01/ex00/java/Zombie.java
@@ -4,14 +4,19 @@ public class Zombie {
     private String name;
     private int age;
 
+    public Zombie() {
+        this.name = "undfined";
+        this.age = 0;
+    }
+
     public Zombie(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-   /*  public String toString() {
+    public String toString() {
         return "Zombie " + name + " is " + age + " years old";
-    } */
+    }
     
 }
 
